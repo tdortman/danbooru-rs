1266b5d214e00325b9c4ce4791e9167badfb0875
Tim Dortmann <git@tdortman.com>
2023-03-06 17:25:17 +0100

    fetch amount of pages

--- src/download.rs
+++ src/download.rs
@@ -1,27 +1,46 @@
+use std::env;
 use std::process;
+use std::str::FromStr;
 
 use crate::args::DownloadCommand;
-use urlencoding::encode;
 use anyhow::Result;
+use hyper::{client::HttpConnector, Client, Uri};
+use urlencoding::encode;
 
 pub async fn handle_download(args: DownloadCommand) {
-    let total_pages = match get_total_pages(&args.tags) {
+    let client = Client::new();
+    let _total_pages = match get_total_pages(&args.tags, client).await {
         Ok(x) => x,
-        Err(_) => {
+        Err(e) => {
             eprintln!("No results found for tags: {:?}", args.tags);
+            eprintln!("{e:#?}");
             process::exit(1);
         }
     };
 }
 
-fn get_total_pages(tags: &[String]) -> Result<i32> {
+async fn get_total_pages(tags: &[String], client: Client<HttpConnector>) -> Result<i32> {
     let encoded_tags = tags
         .iter()
         .map(|x| encode(x).into_owned())
         .collect::<Vec<String>>()
         .join("+");
 
-    let query = format!("https://danbooru.donmai.us/posts?tags={encoded_tags}&limit=200");
-    println!("{query}");
+    let mut query = format!("http://danbooru.donmai.us/posts?tags={encoded_tags}&limit=200");
+
+    if env::var("LOGIN_NAME").is_ok() && env::var("API_KEY").is_ok() {
+        query += &format!(
+            "&login={}&api_key={}",
+            env::var("LOGIN_NAME")?,
+            env::var("API_KEY")?
+        );
+    }
+
+
+    let uri = Uri::from_str(&query)?;
+    let response = client.get(uri).await?;
+    let body = hyper::body::to_bytes(response.into_body()).await?;
+    let html_body = String::from_utf8(body.to_vec())?;
+    println!("{:?}", html_body);
     Ok(1)
 }
--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,14 @@ use args::{
     Commands::{Download, Search},
 };
 use clap::Parser;
+use dotenvy::dotenv;
 use download::handle_download;
 use search::handle_search;
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
+    dotenv().ok();
 
     match args.command {
         Download(x) => handle_download(x).await,
