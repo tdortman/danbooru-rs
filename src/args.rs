d31053c286c97529584003bc6cd188c0b1d42042
Tim Dortmann <git@tdortman.com>
2023-03-07 02:45:26 +0100

    cleanup

--- src/args.rs
+++ src/args.rs
@@ -5,7 +5,7 @@ use clap::{Parser, Subcommand};
 #[derive(Parser, Debug)]
 #[clap(
     author = "Tilted Toast",
-    version = "1.0.0",
+    version = "1.0.1",
     about = "A command line interface for Danbooru"
 )]
 pub struct Args {
