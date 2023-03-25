1aac32dc612895a7fb063b91dd275e51b6698e23
Tim Dortmann <git@tdortman.com>
2023-03-25 02:36:27 +0100

    bump version

--- src/args.rs
+++ src/args.rs
@@ -5,7 +5,7 @@ use clap::{Parser, Subcommand};
 #[derive(Parser, Debug)]
 #[clap(
     author = "Tilted Toast",
-    version = "1.0.11",
+    version = "1.0.12",
     about = "A command line interface for Danbooru"
 )]
 pub struct Args {
