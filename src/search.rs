aa4b76c9505950aaa23e5fb1ef9e9baa9b5e2c57
Tim Dortmann <git@tdortman.com>
2023-04-03 01:38:04 +0200

    add comment for search url for future

--- src/search.rs
+++ src/search.rs
@@ -3,3 +3,5 @@ use crate::args::SearchCommand;
 pub fn handle_search(args: &SearchCommand) {
     println!("{args:#?}");
 }
+
+// https://danbooru.donmai.us/tags.json?search[name_ilike]=*annie*&search[order]=count&search[post_count]>0&limit=10
